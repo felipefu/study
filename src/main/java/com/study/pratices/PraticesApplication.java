f964d81b0afd45b56d658134c32325fd88c8a5c9
Felipe Cafaro <felipecafaro@gmail.com>
2021-05-14 14:12:37 -0300

    first commit

---
+++ src/main/java/com/study/pratices/PraticesApplication.java
@@ -0,0 +1,13 @@
+package com.study.pratices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticesApplication.class, args);
+	}
+
+}
