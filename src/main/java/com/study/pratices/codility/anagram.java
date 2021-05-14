22405499085a926a9874a8b67b860fe6c9ff979d
Felipe Cafaro <felipecafaro@gmail.com>
2021-05-14 14:42:42 -0300

    tests for anagram

---
+++ src/main/java/com/study/pratices/codility/anagram.java
@@ -0,0 +1,39 @@
+package com.study.pratices.codility;
+
+public class anagram {
+
+    public static boolean nestedAnagrams(String phraseInput, String phraseAnagram) {
+
+        String[] inputWords = phraseInput.split(" ");
+        String[] anagramWords = phraseAnagram.split(" ");
+
+        for (String inputWord : inputWords) {
+            boolean wordHasMatch = false;
+            for (String anagramWord : anagramWords) {
+
+                if (inputWord.length() == anagramWord.length()) {
+
+                    String inputWordAux = inputWord;
+
+                    for (char letter : anagramWord.toCharArray()) {
+
+                        if (inputWordAux.contains(String.valueOf(letter))) {
+                            inputWordAux = inputWordAux.replaceFirst(String.valueOf(letter), "");
+                        } else {
+                            break;
+                        }
+                    }
+                    if (inputWordAux.length() == 0) {
+                        wordHasMatch = true;
+                        break;
+                    }
+                }
+            }
+            if (!wordHasMatch) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
