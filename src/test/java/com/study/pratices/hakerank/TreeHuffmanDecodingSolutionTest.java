fb56815438c3c50ec9d4a79e7b8037ce5965bc65
Felipe Cafaro <felipecafaro@gmail.com>
2021-05-15 21:07:08 -0300

    treeHuffman resolution

--- src/test/java/com/study/pratices/hakerank/TreeHuffmanDecodingSolutionTest.java
+++ src/test/java/com/study/pratices/hakerank/TreeHuffmanDecodingSolutionTest.java
@@ -14,7 +14,7 @@ public class TreeHuffmanDecodingTest {
     Decoding decode = new Decoding();
 
     assertEquals("ABACA", decode.decode("1001011", generateTree("ABACA")));
-//    assertEquals("ABRACADABRA", decode.decode("01111001100011010111100", generateTree("ABRACADABRA")));
-//    assertEquals("A", decode.decode("0", generateTree("A")));
+    assertEquals("ABRACADABRA", decode.decode("01111001100011010111100", generateTree("ABRACADABRA")));
+    assertEquals("A", decode.decode("0", generateTree("A")));
   }
 }
